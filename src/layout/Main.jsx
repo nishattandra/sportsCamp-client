7a588da21e9d5e89bcb0882855038cb7605178f8
nishattandra <nishattandra2001@gmail.com>
2023-06-13 22:28:24 +0600

    Navbar is added

---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Navbar from '../pages/Shared/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Main;
