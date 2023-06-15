6dda0b26d8c0fec26e66c8e58c98d0fe2f5370ab
nishattandra <nishattandra2001@gmail.com>
2023-06-15 18:07:51 +0600

    manage users added

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Routes.jsx'
 import AuthProvider from './Providers/AuthProviders'
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient(); 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='bg-gray-300 max-w-screen-xl mx-auto'>
     <React.StrictMode>
-    <AuthProvider>
-        <RouterProvider router={router} />
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </AuthProvider>
     </React.StrictMode>
   </div>
--- src/pages/Dashboard/Admin/Manageuser.jsx
+++ src/pages/Dashboard/Admin/Manageuser.jsx
@@ -1,9 +1,68 @@
 // import React from 'react';
 
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hook/useAxiosSecure";
+
 const Manageuser = () => {
+    const [axiosSecure] = useAxiosSecure();
+    const { data: registeredusers = [], refetch } = useQuery(['users'], async () => {
+        const res = await axiosSecure.get('/admin/registeredusers')
+        return res.data;
+    })
+    const updateType = async(id, type) => {try {
+            const response = await axiosSecure.patch(`/admin/typeupdate/${id}?type=${type}`);
+            console.log(response)
+            refetch()
+          } catch (error) {
+           console.log(error)
+          }
+    }
+
     return (
-        <div>
-            Manage Users
+        <div className="overflow-x-auto mt-24">
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>Photo</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Make Admin</th>
+                        <th>Make Instructor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        registeredusers.map(user =>
+                            <tr key={user._id}>
+                                <td>
+                                    <div className="mask mask-squircle w-12 h-12">
+                                        <img src={user.photo} alt="Avatar Tailwind CSS Component" />
+                                    </div>
+                                </td>
+                                <td>
+                                    <h1>{user.name}</h1>
+                                </td>
+                                <td>
+                                <h1>{user.email}</h1>
+                                </td>
+                                <td>
+                                <h1>{user.type?user.type:'student'}</h1>
+                                </td>
+                                <td>
+                                <button disabled={user.type === 'admin'} onClick={() => updateType(user._id, 'admin')} className="btn btn-error">Make Admin</button>
+                                </td>
+                                <td>
+                                <button disabled={user.type === 'instructor'} onClick={() => updateType(user._id, 'instructor')} className="btn btn-success">Make Instructor</button>
+                                </td>
+                            </tr>)
+                    }
+
+
+                </tbody>
+
+            </table>
         </div>
     );
 };
